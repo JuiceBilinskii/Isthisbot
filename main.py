079ddfc1300e94cf304d076da021b9700b06cbf3
Bilinskii <77838296+JuiceBilinskii@users.noreply.github.com>
2021-07-05 12:10:56 +0000

    Updated the code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import re
 import telebot
 
-KEYS = {'ира', 'иры', 'ире', 'иру', 'ирой', 'ирина', 'ирины', 'ирине', 'ирину', 'ириной', 'ирочка', 'ирка', 'ирусик', 'ириночка', 'сыр', 'иришка', 'ириска'}
+KEYS = {'ира', 'иры', 'ире', 'иру', 'ирой', 'ирина', 'ирины', 'ирине', 'ирину', 'ириной'}
 BOT_TOKEN = os.environ['BOT_TOKEN']
 bot = telebot.TeleBot(BOT_TOKEN)
 
@@ -11,5 +11,5 @@ def get_text_messages(message):
   words = set(re.findall(r'\w+', message.text.lower()))
   if (KEYS & words):
     bot.send_message(message.from_user.id, 'Это та, кто вонюче срет?')
-
+      
 bot.polling()
